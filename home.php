419e92ce9e74aca88422a3082485dc7f2c297d0b
RICO <puiuria64@gmail.com>
2026-01-06 09:03:10 +0700

    Update home.php

--- home.php
+++ home.php
@@ -22,7 +22,7 @@
 			</td>
 			<td width="190" bgcolor="#FFFF66"><ul>
 				<li><a href="home.php">หน้าแรก</li>
-				<li><a href="room.php">ห้องพัก&ราคา</li>
+				<li><a href="room.php">ห้องพัก&ราคาาาา</li>
  branch3
 				<li><a href="facilitate.php">สิ่งอำนวยความสะดวกddddd</li>
 
